fb75931ff743e5290797aa0817edf904cf1d68b5
keewoong nam <keewoong.nam@flicspy.com>
2022-07-22 16:31:52 +0900

    Try

--- library/views.py
+++ library/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from library.models import Book, Human
+from library.serializers import BookSerializer, HumanSerializer
 
-# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all().order_by('title')
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class HumanViewSet(viewsets.ModelViewSet):
+    queryset = Human.objects.all()
+    serializer_class = HumanSerializer
+    permission_classes = [permissions.IsAuthenticated]
