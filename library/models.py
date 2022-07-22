eff9077795a41ba5561064dc2b1a3bfe79fde416
keewoong nam <keewoong.nam@flicspy.com>
2022-07-22 17:41:13 +0900

    method field working

--- library/models.py
+++ library/models.py
@@ -9,3 +9,12 @@ class GcpServiceAccount(models.Model):
     workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE, related_name='gcp_service_accounts')
     name = models.CharField(max_length=200)
     age = models.IntegerField(default=0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["workflow",],
+                name="workflow_unique"
+            ),
+    ]
+
--- library/serializers.py
+++ library/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Workflow, GcpServiceAccount
-
+import json
 
 class GcpServiceAccountSerializer(serializers.ModelSerializer):
     # workflow = WorkflowSerializer()
@@ -10,8 +10,15 @@ class GcpServiceAccountSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class WorkflowSerializer(serializers.ModelSerializer):
-    gcp_service_accounts = GcpServiceAccountSerializer(many=True, read_only=True)
+    # gcp_service_accounts = GcpServiceAccountSerializer(many=True, read_only=True)
 
+    gcp_service_account = serializers.SerializerMethodField()
+
+    def get_gcp_service_account(self, obj):
+        gsa = GcpServiceAccount.objects.filter(workflow_id=obj.id).first()
+        print(gsa.name)
+        print(GcpServiceAccountSerializer(gsa).data)
+        return GcpServiceAccountSerializer(gsa).data
 
     # gcp_service_account = GcpServiceAccountSerializer(queryset=GcpServiceAccount.objects.filter())
     class Meta:
