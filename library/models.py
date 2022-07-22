863a6bf5a2403445e25fa96e0e2f251ba61f9efd
keewoong nam <keewoong.nam@flicspy.com>
2022-07-22 17:19:54 +0900

    Related field working

--- library/models.py
+++ library/models.py
@@ -6,6 +6,6 @@ class Workflow(models.Model):
 
 
 class GcpServiceAccount(models.Model):
-    workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE)
+    workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE, related_name='gcp_service_accounts')
     name = models.CharField(max_length=200)
     age = models.IntegerField(default=0)
--- library/serializers.py
+++ library/serializers.py
@@ -2,13 +2,18 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Workflow, GcpServiceAccount
 
-class WorkflowSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Workflow
-        fields = '__all__'
-
 
 class GcpServiceAccountSerializer(serializers.ModelSerializer):
+    # workflow = WorkflowSerializer()
     class Meta:
         model = GcpServiceAccount
+        fields = '__all__'
+
+class WorkflowSerializer(serializers.ModelSerializer):
+    gcp_service_accounts = GcpServiceAccountSerializer(many=True, read_only=True)
+
+
+    # gcp_service_account = GcpServiceAccountSerializer(queryset=GcpServiceAccount.objects.filter())
+    class Meta:
+        model = Workflow
         fields = '__all__'
